7a6d146472da99a80435173435fd526bfa86ff2b
suyuer123 <1741031871@qq.com>
2023-04-18 04:24:17 +0800

    Add files via upload

---
+++ v3.2.0.3/Ext/ChromeExtension/content.js
@@ -0,0 +1,32 @@
+var productID = "com.cfca.seceditctl.srcbcz";
+var extensionName = productID + ".extension";
+var reqEventName  = productID + ".request";
+var respEventName = productID + ".response";
+var cbEventName   = productID + ".callback";
+
+var isInstalledNode = document.createElement('div');
+isInstalledNode.id = extensionName;
+document.body.appendChild(isInstalledNode);
+
+chrome.runtime.onMessage.addListener(
+    function (request) {
+        try {
+            if (request.function == "JSCallback")
+            {
+                var evtTest = new CustomEvent(cbEventName, { detail: JSON.stringify(request) });
+                document.dispatchEvent(evtTest);
+                return true;
+            }
+        }
+        catch (e) {
+            var resultObj = new Object();
+            resultObj.errorcode = 1;
+            resultObj.result = e.message;
+            
+            var evtTest = new CustomEvent(cbEventName, { detail: JSON.stringify(resultObj) });
+            document.dispatchEvent(evtTest);
+
+            return true;
+        }
+    }
+);
